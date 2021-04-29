838139fe00d195428fbb5b5f85d09839676781e6
Olesia Maslennikova <olesya_korovina_1993@mail.ru>
2021-04-29 16:23:43 +0300

    extensionVC

--- APIDoubleProject/Controllers/ViewController.swift
+++ APIDoubleProject/Controllers/ViewController.swift
@@ -20,10 +20,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         return locManager
     }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBOutlet weak var weatherIconImageView: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     
@@ -32,5 +28,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        networkWeatherManager.onCompletion = { [weak self]
+            currentWeather in
+            guard let self = self else {return}
+            self.updateIntergaceWith(weather: currentWeather)
+        }
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.requestLocation()
+        }
+        
+    }
+    //обновить интерфейс с помощью...
+    func updateIntergaceWith(weather: CurrentWeather){
+        //очередь отправки
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.iconSystemName)
+        }
+        
+    }
+    
 }
+
+extension ViewController {
+    func locationManager(_manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { //обновление локации
+        guard let location = locations.last else { return}
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fetchCurrentWeather(forRequestType: .coordinate(latitude: latitude, longitude: longitude))
+    }
+    func locationManager(_manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
 
